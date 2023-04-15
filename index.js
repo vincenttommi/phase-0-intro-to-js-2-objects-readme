2b289de4adf0fb3c75e29be55662e30a52759b7b
vincenttommi <vincenttommi@gmail.com>
2023-04-15 12:49:20 +0300

    objects in  javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+//Identify javascript objects
+//access values stored in the objects
+//learning about js objects methods
+
+
+const address = ["11 Broadway","2nd Floor","New York","NY","1004"];
+
+
+
+address[1] = "3rd Floor";
+
+
+console.log(address);
